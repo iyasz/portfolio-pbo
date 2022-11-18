bf077ade9412bea52d5ff3ef2630ecb1b8c38427
Yasz Avellia <zakamaragames@gmail.com>
2022-11-18 21:51:53 +0700

    tahap 1clear

--- index.php
+++ index.php
@@ -6,11 +6,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;600&display=swap" rel="stylesheet">
 </head>
+
+<style>
+    .fw-600 {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+    }
+
+    .fw-300 {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 300;
+    }
+
+    .title-say {
+        font-size: 45px;
+    }
+
+    .title-name {
+        font-size: 60px;
+        letter-spacing: 1.5px;
+    }
+
+    .bg-first {
+        background-color: #FFE593;
+    }
+
+    .bg-last {
+        background-color: #96E5CC;
+    }
+</style>
 
 <body>
 
-    <nav class="navbar navbar-dark bg-dark fixed-top">
+    <!-- dekstop view  -->
+
+    <nav class="navbar navbar-expand-sm bg-transparent d-lg-block d-md-block d-none py-4">
+        <div class="container">
+            <a class="navbar-brand fw-600" href="#">Yasz!</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <nav class="navbar navbar-dark bg-dark d-lg-none d-md-none d-block">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Offcanvas dark navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
@@ -51,6 +110,26 @@
             </div>
         </div>
     </nav>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-7">
+                <p class="title-say fw-300 "><img src="https://static.wixstatic.com/media/269653_f8294923a5304079bed95d039fa9a193~mv2.gif" alt="bandar" width="60px" class="rounded-circle"> , I'm</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-10">
+                <div class="">
+                    <h1 class="title-name fw-600"><span class="bg-first"> Muhammad</span> <span class="bg-last">Ilyasa</span></h1>
+                </div>
+                <!-- <div class="">
+                    <h1>Ilyasa</h1>
+                </div> -->
+            </div>
+        </div>
+    </div>
+
+    <!-- mobile view  -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
